d97dd3502fc818565faab4ea969d418ac54909de
LionPony <146934556+LionPony@users.noreply.github.com>
2024-11-17 09:32:02 +0900

    Title: 장학금, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241117/장학금/scholarship.py
+++ 241117/장학금/scholarship.py
@@ -1,9 +1,6 @@
 mid, end = map(int, input().split())
 scholarship = 0
-quilfy = False
 if mid >= 90:
-    quilfy = True
-if quilfy:
     if end >= 95:
         scholarship += 100000
     elif end >= 90:
